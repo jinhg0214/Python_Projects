5795619519263372126a9da477403e888f50f808
Jin Haeng Lee <70011316+jinhg93@users.noreply.github.com>
2023-02-08 16:05:09 +0900

    퀘존 자동 출석

--- Selenium/AutoLogin/main.py
+++ Selenium/AutoLogin/main.py
@@ -7,37 +7,62 @@ options.add_experimental_option("excludeSwitches", ["enable-logging"])
 driver = webdriver.Chrome(options=options)
 
 # ID/PW 정의 부분
-# 일단은 콘솔로 입력받는다
+# 일단은 콘솔로 입력받기
 user_id, user_pw = input('Input ID / PW : ').split()
 
-# 1. tcafe
-# driver.get('https://tcafe2a.com/')
-# driver.implicitly_wait(3)
+# 1. tcafe (00:00)
+driver.get('https://tcafe2a.com/')
+driver.implicitly_wait(3)
 
-# # 1-1. 로그인 파트
-# driver.find_element(By.NAME, 'mb_id').send_keys(user_id) # 태그의 name값으로 추출
-# driver.find_element(By.NAME, 'mb_password').send_keys(user_pw) # 
-# driver.find_element(By.XPATH, '//*[@id="ol_before"]/form/fieldset/div[1]/span[2]/button').click()
+# 1-1. 로그인 파트
+driver.find_element(By.NAME, 'mb_id').send_keys(user_id) # 태그의 name값으로 추출
+driver.find_element(By.NAME, 'mb_password').send_keys(user_pw) # 
+driver.find_element(By.XPATH, '//*[@id="ol_before"]/form/fieldset/div[1]/span[2]/button').click()
  
-# # 1-2. 출석
-# driver.get('https://tcafe2a.com/community/attendance')
-# driver.find_element(By.XPATH, '//*[@id="cnftjr"]/div/form/table/tbody/tr/td/img').click()
+# 1-2. 출석
+driver.get('https://tcafe2a.com/community/attendance')
+driver.find_element(By.XPATH, '//*[@id="cnftjr"]/div/form/table/tbody/tr/td/img').click()
 
-# 2. netpx
+# 2. netpx (00:00)
 driver.get('https://www.netpx.co.kr/app/member/login?target_url=%2Fapp%2F')
 driver.implicitly_wait(3) # 3초간 로딩을 기다린다
 
 # 2-1. 로그인 파트
 driver.find_element(By.XPATH, '//*[@id="form"]/div[1]/input').send_keys(user_id) # 태그의 name값으로 추출
-driver.find_element(By.XPATH, '//*[@id="form"]/div[2]/input').send_keys(user_pw[:-1]) # 비번 특문제거 
+driver.find_element(By.XPATH, '//*[@id="form"]/div[2]/input').send_keys(user_pw) # 비번 특문제거 
 driver.find_element(By.XPATH, '//*[@id="form"]/div[3]/input').click()
 
 # 2-2. 출석 파트
 driver.get('https://www.netpx.co.kr/app/event/attend_check/8')
 driver.find_element(By.XPATH, '//*[@id="flashArea"]/form/div[1]/div[2]/a/img').click()
 
-# 3. 퀘이사존
+# alert 창이 뜨는 경우, 확인을 눌러서 끔. 없으면 pass
+try :
+    alert = driver.switch_to.alert
+    alert.accept()
+    
+except :
+    pass
 
+# 3. 퀘이사존 (09:00)
+driver.get('https://quasarzone.com/login?nextUrl=https://quasarzone.com/')
+driver.implicitly_wait(3) 
 
-# N. 드라이버 종료
+driver.find_element(By.XPATH, '//*[@id="login_id"]').send_keys(user_id)
+driver.find_element(By.XPATH, '//*[@id="password"]').send_keys(user_pw)
+driver.find_element(By.XPATH, '//*[@id="frm"]/div/div[1]/p/a').click()
+
+driver.get('https://quasarzone.com/users/attendance')
+driver.implicitly_wait(3)
+
+driver.find_element(By.CLASS_NAME, 'active2').click()
+driver.implicitly_wait(3)
+
+try :
+    alert = driver.switch_to.alert
+    alert.accept()
+except :
+    pass
+
+# 99. 드라이버 종료
 driver.quit()
